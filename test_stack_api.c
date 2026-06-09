fe13ba2d875651d6254f3ca43f1ccf9352092cd2
Boris Kantorovich <bkantoro@c3a1c6.42berlin.de>
2026-06-09 14:29:26 +0200

    modified tests

--- test_stack_api.c
+++ test_stack_api.c
@@ -6,11 +6,19 @@
 /*   By: bkantoro <bkantoro@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 13:34:00 by bkantoro          #+#    #+#             */
-/*   Updated: 2026/06/09 14:14:10 by bkantoro         ###   ########.fr       */
+/*   Updated: 2026/06/09 14:27:46 by bkantoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	test_print(t_stack *test, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d\n", test->numbers[i]);
+	}
+}
 
 int	main(void)
 {
@@ -19,14 +27,10 @@ int	main(void)
 	int test_nb[] = {100, 213, 12, INT_MAX, INT_MIN};	
 	test = init_stack(n);
 	fill_stack(test, test_nb, n);
-	for (int i = 0; i < n; i++)
-	{
-		printf("%d\n", test->numbers[i]);
-	}
+	test_print(test, n);
 	sa(test);	
-	for (int i = 0; i < n; i++)
-	{
-		printf("%d\n", test->numbers[i]);
-	}
+	test_print(test, n);
+	sb(test);	
+	test_print(test, n);
 	clear_stack(test);
 }
