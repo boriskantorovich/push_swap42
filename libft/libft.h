381c3911d241927a1744d577e5a1e5ae58f1cad5
boris <bkantreg@gmail.com>
2026-06-12 21:21:22 +0200

    added ft_swap to libft

--- libft/libft.h
+++ libft/libft.h
@@ -27,7 +27,7 @@ int					ft_isdigit(int c);
 int					ft_isalnum(int c);
 int					ft_isprint(int c);
 int					ft_isascii(int c);
-
+void					ft_swap(int *a, int *b);
 size_t				ft_strlen(const char *s);
 void				*ft_memset(void *s, int c, size_t n);
 void				ft_bzero(void *s, size_t n);
