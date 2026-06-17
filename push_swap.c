8be78c28f80823bc63249ddd43d567c713b9ae36
Miroslau Ilnicki <milnicki@c3a1c7.42berlin.de>
2026-06-17 17:35:30 +0200

    disorder segfault fixed.

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@ int	push_swap(int *input, int size, int flag)
 	t_stack			*b;
 	t_ops			*ops;
 	int				isbench;
-	const double	disorder = compute_disorder(a);
+	double			disorder;
 
 	if (!input)
 		return (-1);
@@ -21,6 +21,7 @@ int	push_swap(int *input, int size, int flag)
 	isbench = ((flag - 3) % 2 == 0);
 	ops = init_ops(flag, isbench);
 	fill_stack(a, input, size);
+	disorder = compute_disorder(a);
 	dispatch(a, b, ops, flag);
 	bench(ops, disorder, isbench);
 	clear_stack(a);
