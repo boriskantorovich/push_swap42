d144ea7124c7608e2288e25a3f4f27001b41b462
boris <bkantreg@gmail.com>
2026-06-18 15:22:56 +0200

    input check fixin

--- main.c
+++ main.c
@@ -6,19 +6,24 @@ int	main(int argc, char **argv)
 	int			flag;
 	int			*input;
 
-	if (size > 1)
+	if (size <= 0)
+		return (0);
+	flag = parse_flags(argv);
+	input = validate_values(argc, argv, size);
+	if (!input)
+		return (print_error());
+	if (validate_array(input, size) == 2)
 	{
-		flag = parse_flags(argv);
-		input = validate_values(argc, argv, size);
-		if (!input)
-			return (print_error());
-		if (validate_array(input, size) == 2)
-			return (print_error());
-		if (validate_array(input, size) == 0)
-			return (0);
-		rank_array(input, size);
-		push_swap(input, size, flag);
 		free(input);
+		return (print_error());
 	}
+	if (validate_array(input, size) == 0)
+	{
+		free(input);
+		return (0);
+	}
+	rank_array(input, size);
+	push_swap(input, size, flag);
+	free(input);
 	return (0);
 }
