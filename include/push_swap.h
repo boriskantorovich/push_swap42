ac477a0855b3d6e1d1de6e5c5016d4f4208ff4aa
boris <bkantreg@gmail.com>
2026-06-18 15:22:35 +0200

    format fixes

--- include/push_swap.h
+++ include/push_swap.h
@@ -3,17 +3,16 @@
 
 # include "limits.h"
 # include "string.h"
-# include "bsd/string.h"
 # include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
 
 typedef struct s_stack
 {
-	int	*numbers;
-	int	count;
-	int	size;
-}		t_stack;
+	int		*numbers;
+	int		count;
+	int		size;
+}			t_stack;
 
 typedef struct s_ops
 {
@@ -22,27 +21,27 @@ typedef struct s_ops
 	int		flag;
 	char	*strategy;
 	int		isbench;
-}		t_ops;
+}			t_ops;
 
+# include "ft_printf.h"
 # include "operations.h"
 # include "strategies.h"
 # include "utils.h"
-# include "ft_printf.h"
 
-int		push_swap(int *input, int size, int flag);
+int			push_swap(int *input, int size, int flag);
 
-int		parse_flags(char **argv);
-int		parse_offset(int flag);
-int		validate_array(int *input, int size);
-int		*validate_values(int argc, char **argv, int size);
+int			parse_flags(char **argv);
+int			parse_offset(int flag);
+int			validate_array(int *input, int size);
+int			*validate_values(int argc, char **argv, int size);
 
-int		rank_array(int *input, int size);
+int			rank_array(int *input, int size);
 
-t_stack	*init_stack(int n);
-int		fill_stack(t_stack *stack, int *input, int n);
-int		clear_stack(t_stack *stack);
-void	print_bench(t_ops *o, double disorder);
-void	bench(t_ops *o, double disorder, int isbench);
-double	compute_disorder(t_stack *a);
+t_stack		*init_stack(int n);
+int			fill_stack(t_stack *stack, int *input, int n);
+int			clear_stack(t_stack *stack);
+void		print_bench(t_ops *o, double disorder);
+void		bench(t_ops *o, double disorder, int isbench);
+double		compute_disorder(t_stack *a);
 
 #endif
