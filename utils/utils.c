a93651f59055762ed88caf859d2e76760a0e83aa
Miroslau Ilnicki <milnicki@c3a1c1.42berlin.de>
2026-06-23 11:28:21 +0200

    norm

--- utils/utils.c
+++ utils/utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_bench.c                                      :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bkantoro <bkantoro@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/19 13:11:43 by bkantoro          #+#    #+#             */
-/*   Updated: 2026/06/19 13:17:09 by bkantoro         ###   ########.fr       */
+/*   Updated: 2026/06/23 11:28:03 by milnicki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
@@ -23,7 +23,8 @@ void	bench(t_ops *o, double disorder, int isbench)
 		ft_printf("[bench] disorder:  %d.%d%d%%\n"\
 			"[bench] strategy:  %s\n[bench] total_ops:  %d\n"\
 			"[bench] sa:  %d  sb:  %d  ss:  %d  pa:  %d  pb:  %d\n"\
-			"[bench] ra:  %d  rb:  %d  rr:  %d  rra:  %d  rrb: %d  rrr:  %d\n", whole, fraction / 10, fraction % 10, \
+			"[bench] ra:  %d  rb:  %d  rr:  %d  rra:  %d  rrb: %d  rrr:  %d\n"\
+			, whole, fraction / 10, fraction % 10, \
 			o->strategy, o->total_ops, o->counter[OP_SA], \
 			o->counter[OP_SB], o->counter[OP_SS], o->counter[OP_PA], \
 			o->counter[OP_PB], o->counter[OP_RA], o->counter[OP_RB], \
@@ -32,7 +33,7 @@ void	bench(t_ops *o, double disorder, int isbench)
 	}
 }
 
-int		print_error(void)
+int	print_error(void)
 {
 	write(2, "Error\n", 6);
 	return (2);
